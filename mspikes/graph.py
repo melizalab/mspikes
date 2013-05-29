c7045f0ec35ab1c99b5e0f9d7fe567287511e511
Dan Meliza <dan@meliza.org>
2013-05-29 17:24:33 -0500

    parse graph spec using ast

--- mspikes/graph.py
+++ mspikes/graph.py
@@ -16,24 +16,55 @@ def node_regex():
     return re.compile(r"^" symbol r"\s+" symbol)
 
 
-
 def parse_graph(graphspec):
     """Parse an mspikes graph specification.
 
-    The graph specification comprises a list of strings that must match the
-    following regular expression.
+    The graph specification comprises a list of strings that must have the
+    syntax of a python assignment with the form:
 
-    node = node-name SP node-spec
-    node-spec = node-type *node-arg
-    node-arg  = SP (node-keyword SP node-value) / (node-value)
-    node-value = numeric-value / string-value
-    node-name = symbol
-    node-type = symbol
-    node-keyword = %x3A symbol  ; lisp-style keyword
-    symbol = ALPHA *(ALPHA / DIGIT / %x5F )
-    numeric-value
+    node_name = node_type(*sources, **parameters)
+
+    Sources arguments are only requires for Sink nodes. Each source may be
+    - the name of another node
+    - a tuple, (node_name, chunk_filter),
+      where chunk_filter is either "event" or "sampled"
+
+    Keyword arguments are interpreted as values passed to node_type's
+    constructor.
 
     """
+    import ast
+
+    tree = ast.parse(graphspec)
+    for stmt in tree.body:
+        if not isinstance(stmt, ast.Assign):
+            continue
+        if not len(stmt.targets) == 1:
+            raise SyntaxError("only one node name allowed: %s" % ast.dump(stmt))
+        if not isinstance(stmt.value, ast.Call):
+            raise SyntaxError("invalid format for node specification: %s" % ast.dump(stmt.value))
+
+        node_name = stmt.targets[0].id
+        node_type = stmt.value.func.id
+
+        node_sources = []
+        for arg in stmt.value.args:
+            if isinstance(arg, (ast.Tuple, ast.List)):
+                if not len(arg.elts) == 2:
+                    raise SyntaxError("source tuple must have two elements: %s" % ast.dump(arg))
+                if not isinstance(arg.elts[0], ast.Name):
+                    raise SyntaxError("first source element must be a symbol: %s" % ast.dump(arg))
+                node_sources.append(arg.elts)
+            elif isinstance(arg, ast.Name):
+                node_sources.apend((arg,))
+            else:
+                raise SyntaxError("invalid source specification: %s" % ast.dump(arg))
+
+        node_params = dict((k.arg, ast.literal_eval(k.value)) for k in stmt.value.keywords)
+
+
+
+
 
 # Variables:
 # End:
