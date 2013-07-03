0f4e2ad032c256db6365f32919850b7551d5ad32
Dan Meliza <dan@meliza.org>
2013-07-03 17:50:51 -0500

    more flexible exponential smoother

--- mspikes/modules/neural_filter.py
+++ mspikes/modules/neural_filter.py
@@ -5,35 +5,47 @@
 Copyright (C) 2013 Dan Meliza <dmeliza@gmail.com>
 Created Wed Jul  3 13:22:29 2013
 """
+from __future__ import division
 import logging
 from mspikes.types import Source, Sink, DataBlock
 
 _log = logging.getLogger(__name__)
 
 
-def exponential_smoother(data, blocksize):
+def exponential_smoother(data, window_size, init=None):
+    """Calculate moving window statistics of data using an exponential smoother
+
+    window_size - the integration window
+    init - a tuple giving (initial value, number of points) for initializing the
+           smoother. If not supplied, the first window_size points will be used.
+    """
+
     from numpy import zeros
     assert data.ndim == 1
     avg = zeros(data.shape)
-    avg[:blocksize] = data[:blocksize].mean()
-    for i in xrange(blocksize, data.size):
-        avg[i] = (avg[i-1] * blocksize + data[i]) / (blocksize + 1)
+
+    if init is None:
+        init = data[:window_size].mean()
+        N = window_size
+    else:
+        init, N = init
+
+    avg[0] = (init * N + data[0]) / (N + 1)
+
+    i = 1
+    while i < N:
+        avg[i] = (avg[i-1] * N + data[i]) / (N + 1)
+        i += 1
+
+    while i < window_size:
+        avg[i] = (avg[i-1] * i + data[i]) / (i + 1)
+        i += 1
+
+    while i < data.size:
+        avg[i] = (avg[i-1] * window_size + data[i]) / (window_size + 1)
+        i += 1
 
     return avg
-
-def adaptive_exponential_smoother(data, blocksize, initsize=1):
-    from numpy import zeros
-    assert data.ndim == 1
-
-    avg = zeros(data.shape)
-    nframes = initsize
-    avg[0] = data[:nframes].mean()
-    for i in xrange(1, data.size):
-        avg[i] = (avg[i - 1] * nframes + data[i]) / (nframes + 1)
-        nframes = max(blocksize, max(nframes, i) + 1)
-
-    return avg
-
 
 
 class zscale(Source, Sink):
