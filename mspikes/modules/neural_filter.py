59883cb19a1c88735939fd6181b930951b6151ee
Dan Meliza <dan@meliza.org>
2013-07-03 17:20:30 -0500

    neural_filter: some basic smoother functions

--- mspikes/modules/neural_filter.py
+++ mspikes/modules/neural_filter.py
@@ -11,6 +11,31 @@ from mspikes.types import Source, Sink, DataBlock
 _log = logging.getLogger(__name__)
 
 
+def exponential_smoother(data, blocksize):
+    from numpy import zeros
+    assert data.ndim == 1
+    avg = zeros(data.shape)
+    avg[:blocksize] = data[:blocksize].mean()
+    for i in xrange(blocksize, data.size):
+        avg[i] = (avg[i-1] * blocksize + data[i]) / (blocksize + 1)
+
+    return avg
+
+def adaptive_exponential_smoother(data, blocksize, initsize=1):
+    from numpy import zeros
+    assert data.ndim == 1
+
+    avg = zeros(data.shape)
+    nframes = initsize
+    avg[0] = data[:nframes].mean()
+    for i in xrange(1, data.size):
+        avg[i] = (avg[i - 1] * nframes + data[i]) / (nframes + 1)
+        nframes = max(blocksize, max(nframes, i) + 1)
+
+    return avg
+
+
+
 class zscale(Source, Sink):
     """Centers and rescales time series data, optionally excluding
 
