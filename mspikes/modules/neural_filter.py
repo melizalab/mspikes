0c88a6d1afa55aac731937299ded6fab3a6c8a6c
Dan Meliza <dan@meliza.org>
2013-07-10 13:12:58 -0500

    simplified and abstracted smoother

--- mspikes/modules/neural_filter.py
+++ mspikes/modules/neural_filter.py
@@ -7,28 +7,82 @@ Created Wed Jul  3 13:22:29 2013
 """
 from __future__ import division
 import logging
+import numpy as nx
+from fractions import Fraction
 
 from mspikes import util
 from mspikes.types import Node, DataBlock
+from mspikes.modules.util import coroutine
 
 _log = logging.getLogger(__name__)
 
-def moving_meanvar(x, s, w):
+def moving_meanvar(x, w=0, s=None):
     """Calculating moving mean and variance of a time series
 
     x - input data. 1-dimensional numpy array
 
-    s - previous state: (mean, variance)
-
     w - weight assigned to previous state, relative to the number of elements in
         x. set to 0 to initialize
+
+    s - previous state: (mean, variance)
 
     returns updated mean, variance
 
     """
-    m = (s[0] * w + x.sum()) / (w + x.size)
-    v = (s[1] * w + ((x - m) ** 2).sum()) / (w + x.size)
+    m0,v0 = (w * a for a in s) if w > 0 else (0, 0)
+    m = (m0 + x.sum()) / (w + x.size)
+    v = (v0 + ((x - m) ** 2).sum()) / (w + x.size)
     return (m, v)
+
+
+@coroutine
+def smoother(func, M, S0=None, N0=0):
+    """A coroutine for applying a moving smoother to data arrays
+
+    Given a time series X = {x_0,...,x_N}, calculates the average of
+    func({x_i,...,x_i+N}) and func({x_i-M,...x_i-1}), weighted by N and M.
+
+    func -- a function with signature f(X, S, M) that updates the sliding window
+            statistics according to the following formula:
+
+            (S * M + f(X)) / (M + X.size)
+
+    M -- the number of samples in the sliding window
+
+    S0 -- the initial value of the smoother. If None, this will be estimated
+          from the first M samples.
+
+    N -- the number of samples used to calculate S0
+
+    Usage:
+    >>> g = smoother(mean, 100)
+    >>> g.send(ones(50))
+    None  # returns None until the number of samples is >= M
+    >>> g.send(zeros(50))
+    0.5
+    >>> g.send(ones(1))
+    0.504950495049505
+
+    """
+    from numpy import concatenate, asarray
+
+    queue = []
+    N = N0
+    try:
+        while N < M:
+            # return None while initializing
+            X = asarray((yield))
+            queue.append(X)
+            N += X.size
+        # calculate initial state from full length of the window
+        X = concatenate(queue)
+        S = func(X, N0, S0)
+        while True:
+            # note: send() assigns value to X, then loops and returns S
+            X = asarray((yield S))
+            S = func(X, M, S)
+    except GeneratorExit:
+        pass
 
 
 def exponential_smoother(data, M, step=1, M_0=None, S_0=None):
@@ -57,11 +111,10 @@ def exponential_smoother(data, M, step=1, M_0=None, S_0=None):
            are estimated from the first M_0 samples of the data
 
     """
-    from numpy import zeros, array, asarray
     assert data.ndim == 1
 
     if M_0 is None or M_0 == 0:
-        M_0 = M
+        M_0 = min(M, data.size)
 
     # initial values from data
     if S_0 is None:
@@ -70,15 +123,15 @@ def exponential_smoother(data, M, step=1, M_0=None, S_0=None):
     # unrolled loop for different weightings
     for i in xrange(0, M_0, step):
         S_0 = moving_meanvar(data[i:i+step], S_0, M_0)
-        yield S_0
+        yield S_0, M_0
 
     for i in xrange(M_0, M, step):
         S_0 = moving_meanvar(data[i:i+step], S_0, i+1)
-        yield S_0
+        yield S_0, i+1
 
     for i in xrange(M, data.size, step):
         S_0 = moving_meanvar(data[i:i+step], S_0, M)
-        yield S_0
+        yield S_0, M
 
 
 class rms_exclude(Node):
@@ -126,28 +179,76 @@ class zscale(Node):
     @classmethod
     def options(cls, addopt_f, **defaults):
         addopt_f("--window",
-                 help="duration of sliding window for calculating mean and SD (in s; default %(default).1f)",
+                 help="duration of sliding window (s) for calculating mean and SD (default=%(default).1f)",
                  default=defaults.get('window', 2),
                  type=float,
                  metavar='SEC')
         addopt_f("--step",
-                 help="step size for sliding window (in ms; default=%(default).1f)",
+                 help="step size (ms) for sliding window (default=%(default).0f)",
                  default=defaults.get('step', 100),
                  type=float,
                  metavar='MS')
         addopt_f("--exclude-rms",
-                 help="if set, exclude intervals where RMS is greater than %(metavar)s %% above baseline",
+                 help="if set, exclude intervals where RMS > %(metavar)s%% above baseline",
                  type=float,
-                 metavar='FLOAT')
-
-
-
+                 metavar='F')
+        addopt_f("--exclude-duration",
+                 help="only exclude intervals longer than %(metavar)s ms (default=%(default).0f)",
+                 default=defaults.get('exclude_duration', 100),
+                 metavar='MS')
+        addopt_f("--exclude-baseline",
+                 help="duration of interval (s) used to set RMS baseline (default=%(default).1f)",
+                 default=defaults.get('exclude_baseline', 10),
+                 metavar='SEC')
 
     def __init__(self, **options):
-        util.set_option_attributes(self, options, window=2.0)
+        util.set_option_attributes(self, options, window=2.0, step=100.,
+                                   exclude_rms=None, exclude_duration=100., exclude_baseline=10.)
+        self.last_sample_time = 0    # time of last sample
+        self.nsamples = 0
+        self.stats = (0, 0)
+        self.dt = None
 
-    def send(self, data):
-        pass
+    def send(self, chunk):
+
+        # only operate on time series
+        if not "samples" in chunk.tags:
+            Node.send(self, chunk)
+
+        # check sampling rate consistency
+        # if self.dt != chunk.dt:
+        #     self.dt = chunk.dt
+        #     if self.dt is not None:
+        #         _log.warn("sampling rate of %s changed at offset %s", chunk.id, chunk.offset)
+
+        # adjust weighting of past average for gaps
+        self.nsamples = max(0, self.nsamples - int((chunk.offset - self.last_sample_time) * chunk.dt))
+
+        # convert time windows to sample counts
+        n_window = int(self.window * chunk.dt)
+        n_step = int(self.step * chunk.dt / 1000.)
+
+        # if self.exclude_rms is not None:
+        #     # push chunk into queue while calculating baseline rms
+        #     pass
+
+        data = chunk.data
+        out = nx.zeros(data.size)
+
+        # initialize
+
+        # for i, ((mean, var), nsamples) in enumerate(exponential_smoother(chunk.data, n_window, n_step,
+        #                                                                  self.nsamples, self.stats)):
+        #     rng = slice(i, i + n_step)
+        #     out[rng] = (chunk.data[rng] - mean) / nx.sqrt(var)
+
+        # self.nsamples = nsamples
+        # self.stats = (mean, var)
+        self.last_sample_time = util.samples_to_seconds(data.size, chunk.dt, chunk.offset)
+        print self.last_sample_time
+        # Node.send(self, chunk._replace(data=out))
+        Node.send(self, chunk)
+
 
 
 
--- test/test_neural_filter.py
+++ test/test_neural_filter.py
@@ -15,26 +15,32 @@ from mspikes.modules import neural_filter
 
 def test_exponential_filter():
 
-    data = nx.asarray([100., 50., 150.,])
+    data = nx.asarray([100, 50, 150,])
 
     m_exp = [ 100.    ,   87.5  ,   103.125]
     v_exp = [ 1250., 1289.0625, 1516.11328125]
-    for i,(m,v) in enumerate(neural_filter.exponential_smoother(data, 3, 1)):
-        assert_equal(m, m_exp[i])
-        assert_equal(v, v_exp[i])
-
-    # with one data point from the past
-    m_exp = [ 100.  ,   (100 + 100 + 50.) / 3  , (100 + 100 + 50 + 150) / 4 ]
-    v_exp = [  500.      ,   (500 * 2 + (m_exp[1] - data[1])**2) / 3     ,  1152.7777777777778]
-    for i,(m,v) in enumerate(neural_filter.exponential_smoother(data, 3, 1, 1, (100., 1000.))):
-        assert_equal(m, m_exp[i])
-        assert_equal(v, v_exp[i])
+    g = neural_filter.smoother(neural_filter.moving_meanvar, 3)
+    g.send(data)                # initialize
+    for d, me, ve in zip(data, m_exp, v_exp):
+        m, v = g.send(d)
+        assert_equal(m, me)
+        assert_equal(v, ve)
 
     # with step size 2
     m_exp = [(100 * 3 + 100 + 50.) / 5, (90 * 3 + 150.) / 4]
     v_exp = [(data.var() * 3 + (50-90)**2 + (100-90)**2) / 5, 1511.25]
-    for i,(m,v) in enumerate(neural_filter.exponential_smoother(data, 3, 2)):
+    g = neural_filter.smoother(neural_filter.moving_meanvar, 3)
+    g.send(data)                # initialize
+    for i in xrange(0, 2):
+        j = i * 2
+        m, v = g.send(data[j:j + 2])
         assert_equal(m, m_exp[i])
         assert_equal(v, v_exp[i])
 
-
+    # # with one data point from the past
+    m_exp = [ (100. + 100. + 50) / 3, (100. + 100 + 50 + 150) / 4]
+    g = neural_filter.smoother(neural_filter.moving_meanvar, 3, (100., 1000.), 1)
+    assert_is_none(g.send(data[:1]))
+    for d, me in zip(data[1:], m_exp):
+        m, v = g.send([d])
+        assert_equal(m, me)
